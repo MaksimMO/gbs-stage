605dc80e8098f8fcdcb1b764700e60b58b42289c
DNechyporuk <dmytro.nechyporuk@jpj.com>
2021-03-19 19:59:09 +0200

    add Bootstrap remove login to admin page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,39 +1,30 @@
-@extends('layouts.app')
+@extends('layouts.gbsadmin')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header" style="font-size: 36px; text-align: center;">Список запитів</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('Welcome admin!') }}
-                </div>
             </div>
             <div>
-                <table cellpadding="7" cellspacing="1" border="1">
+                <table class="table table-striped table-bordered">
                     <thead>
                     <tr>
-                        <th>Level</th>
-                        <th>Ім'я</th>
-                        <th>Телефон</th>
-                        <th>Результат дзвінка</th>
-                        <th>Коментар</th>
-                        <th>Дата останніх змін</th>
-                        <th>Змінити</th>
+                        <th scope="col">Level</th>
+                        <th scope="col">Ім'я</th>
+                        <th scope="col">Телефон</th>
+                        <th scope="col">Результат дзвінка</th>
+                        <th scope="col">Коментар</th>
+                        <th scope="col">Дата останніх змін</th>
+                        <th scope="col">Дія</th>
                     </tr>
                     </thead>
                     <tbody>
                     @foreach ($leads as $lead)
                     <tr>
-                        <td>{{ strtoupper($lead['level']) }}</td>
+                        <th scope="row">{{ strtoupper($lead['level']) }}</th>
                         <td>{{ $lead['name'] }}</td>
                         <td>{{ $lead['phone_number'] }}</td>
 
@@ -49,7 +40,11 @@
 
                         <td>{{ isset($lead['description']) ? $lead['description'] : "" }}</td>
                         <td>{{ $lead['updated_at'] }}</td>
-                        <td><a href="{{ URL::to('update-data/'.$lead['id']) }}">Змінити</a></td>
+                        <td>
+                            @if ( ($lead['called_success'] == 0) || ($lead['called_success'] == 1))
+                            <a href="{{ URL::to('update-data/'.$lead['id']) }}">Змінити</a>
+                            @endif
+                        </td>
                     </tr>
                     @endforeach
                     </tbody>
--- resources/views/edit-lead.blade.php
+++ resources/views/edit-lead.blade.php
@@ -5,20 +5,12 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('Welcome admin!') }}
+                <div class="card-header text-center">
+                    <h1>Редагувати запит від {{ $lead['name'] }}</h1>
                 </div>
+
             </div>
             <div>
-                <h1>Редагувати запит від {{ $lead['name'] }}</h1>
 
                 {{ Form::open(['url' => "/update-data/".$lead['id'], 'method' => 'put']) }}
 
@@ -39,7 +31,7 @@
 
                 <div class="form-group">
                     {{ Form::label('called_success', 'Результат дзвінка') }}
-                    {{ Form::select('called_success', ['0' => 'Новий', '1' => 'Не взяли трубку', '2' => 'Завершений дзвінок', '3' => 'Нерелевантні'], array('class' => 'form-control')) }}
+                    {{ Form::select('called_success', ['0' => 'Новий', '1' => 'Не взяли трубку', '2' => 'Успішний дзвінок', '3' => 'Некоректний'], null, array('class' => 'form-control')) }}
                 </div>
 
                 <div class="form-group">
@@ -48,7 +40,7 @@
                 </div>
 
                 {{ Form::submit('Зберегти', array('class' => 'btn btn-primary')) }}
-                <a href="{{url()->previous()}}" class="btn btn-default">Відміна</a>
+                <a href="{{url()->previous()}}" class="btn btn-danger">Відміна</a>
 
                 {{ Form::close() }}
             </div>
