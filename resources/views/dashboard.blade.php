afffbc477bac1879a26a6d6f7b65ec42ac97d9d8
DNechyporuk <dmytro.nechyporuk@jpj.com>
2021-03-22 16:06:54 +0200

    table bg-color

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,10 +6,9 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header" style="font-size: 36px; text-align: center;">Список запитів</div>
-
             </div>
-            <div>
-                <table id="leads" class="table table-bordered">
+            <div class="mt-3">
+                <table id="leads" class="table table-striped table-bordered">
                     <thead>
                     <tr>
                         <th scope="col">Level</th>
@@ -23,19 +22,21 @@
                     </thead>
                     <tbody>
                     @foreach ($leads as $lead)
-                    <tr @if ($lead['called_success'] == 0)
-                            style="background-color:#fff;"
-                        @elseif ($lead['called_success'] == 1)
-                            style="background-color:yellow;"
-                        @elseif ($lead['called_success'] == 2)
-                            style="background-color:#A3F06C;"
-                        @elseif ($lead['called_success'] == 3)
-                            style="background-color:#FF4040;"
-                        @endif >
+                    <tr>
                         <th scope="row">{{ strtoupper($lead['level']) }}</th>
                         <td>{{ $lead['name'] }}</td>
                         <td>{{ $lead['phone_number'] }}</td>
-                        <td data-order="{{ $lead['called_success'] }}">
+                        <td data-order="{{ $lead['called_success'] }}"
+                            @if ($lead['called_success'] == 0)
+                                style="background-color:#fff;"
+                            @elseif ($lead['called_success'] == 1)
+                                style="background-color:yellow;"
+                            @elseif ($lead['called_success'] == 2)
+                                style="background-color:#A3F06C;"
+                            @elseif ($lead['called_success'] == 3)
+                                style="background-color:#FF4040;"
+                            @endif
+                        >
                             @if ($lead['called_success'] == 0)
                                 Новий
                             @elseif ($lead['called_success'] == 1)
