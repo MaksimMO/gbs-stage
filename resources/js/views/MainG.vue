4d438e05565f28b0e1f87d8dda0fd5be77cff98a
Oleksandr Brezdeniuk <v-oleksandr.brezdeniuk@gmail.com>
2021-07-22 23:36:21 +0300

    add Elena_Dementieva.MOV

--- resources/js/views/MainG.vue
+++ resources/js/views/MainG.vue
@@ -170,7 +170,7 @@ import VideoBackground from 'vue-responsive-video-background-player';
                 {id: 7, link: "Olena_Semenchuk.jpg", linkVideo: "Olena_Semenchuk.MOV"},
                 {id: 8, link: "Nechko_Vasil.jpg", linkVideo: "Nechko_Vasil.MOV"},
                 {id: 9, link: "Gornushkina_Julia.jpg", linkVideo: "Gornushkina_Julia.MOV"},
-                {id: 10, link: "image-not-found.jpg"},
+                {id: 10, link: "image-not-found.jpg", linkVideo: "Elena_Dementieva.MOV"},
                 {id: 11, link: "Nina_Shevchuk.jpg", linkVideo: "Nina_Shevchuk.MOV"},
                 {id: 12, link: "image-not-found.jpg"},
                 {id: 13, link: "image-not-found.jpg"},
